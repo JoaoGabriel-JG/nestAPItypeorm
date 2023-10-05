99ee8a4be8c63efc751f79832e9ef6884c58554f
JoaoGabriel-JG <joao.nitsus@gmail.com>
2023-10-05 16:31:30 -0300

    att migration

--- typeorm/migrations/1696529409688-Migrate.ts
+++ typeorm/migrations/1696529409688-Migrate.ts
@@ -12,43 +12,43 @@ export class Migrate1696529409688 implements MigrationInterface {
                     isPrimary: true,
                     isGenerated: true,
                     generationStrategy: 'increment',
-                    unsigned: true,
+                    unsigned: true
                 },
                 {
                     name: 'name',
                     type: 'varchar',
-                    length: '63',
+                    length: '63'
                 },
                 {
                     name: 'email',
                     type: 'varchar',
                     length: '127',
-                    isUnique: true,
+                    isUnique: true
                 },
                 {
                     name: 'password',
                     type: 'varchar',
-                    length: '127',
+                    length: '127'
                 },
                 {
                     name: 'birthAt',
                     type: 'date',
-                    isNullable: true,
+                    isNullable: true
                 },
                 {
                     name: 'role',
                     type: 'int',
-                    default: '1',
+                    default: '1'
                 },
                 {
                     name: 'createdAt',
                     type: 'timestamp',
-                    default: 'CURRENT_TIMESTAMP()',
+                    default: 'CURRENT_TIMESTAMP'
                 },
                 {
                     name: 'updatedAt',
                     type: 'timestamp',
-                    default: 'CURRENT_TIMESTAMP()',
+                    default: 'CURRENT_TIMESTAMP'
                 }
             ]
         }))
